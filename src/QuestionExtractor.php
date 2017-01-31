effe1a14301e058a08f3b65a418a9644bdb84c5c
Jeff Puckett <jeff@jeffpuckett.com>
2017-01-31 23:06:07 +0000

    delete from tempq table

--- src/QuestionExtractor.php
+++ src/QuestionExtractor.php
@@ -2,5 +2,27 @@
 
 class QuestionExtractor
 {
-    
+    protected static $pdo;
+
+    /**
+     * Run the extractor script.
+     *
+     * @return void
+     */
+    public static function run()
+    {
+        static::$pdo = EnhancedContainer::pdo();
+        static::clean();
+    }
+
+    /**
+     * Delete all records from the temporary questions table.
+     *
+     * @return void
+     */
+    protected static function clean()
+    {
+        $sql = "DELETE FROM [isys4283].[dbo].[tempq]";
+        static::$pdo->exec($sql);
+    }
 }
