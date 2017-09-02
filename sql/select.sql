0cf815b664876d8212ac627d44ecee715f2586f4
Jeff Puckett <jeff@jeffpuckett.com>
2017-09-02 02:40:32 +0000

    add aggregate count

--- sql/select.sql
+++ sql/select.sql
@@ -1,12 +1,31 @@
 DECLARE @date AS DATETIME = '2017-08-29'
 
-SELECT u.first_name, u.last_name,
-    q.id, q.question,
-    a.id, a.user_login, a.answer
-FROM users u
-LEFT JOIN questions q
-  ON u.username = q.user_login
+SELECT q.id, q.user_login, q.question,
+       a.id, a.user_login, a.answer
+FROM questions q
 LEFT JOIN answers a
   ON q.id = a.question_id
 WHERE q.created_at >= @date
+ORDER BY q.id
+
+SELECT u.username, u.first_name, u.last_name,
+    COUNT(q.id) AS 'Questions',
+    COUNT(a.id) AS 'Answers'
+FROM users u
+LEFT JOIN (
+    SELECT *
+    FROM questions
+    WHERE created_at >= @date
+) q
+  ON u.username = q.user_login
+LEFT JOIN (
+    SELECT a.*
+    FROM questions q
+    JOIN answers a
+      ON q.id = a.question_id
+    WHERE q.created_at >= @date
+      AND q.user_login <> a.user_login
+) a
+  ON u.username = a.user_login
+GROUP BY u.id, u.username, u.first_name, u.last_name
 ORDER BY u.last_name
