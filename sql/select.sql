0fef8b0b7f3dde65729fab8da13f9a653fe66787
Jeff Puckett <jeff@jeffpuckett.com>
2017-09-02 01:53:30 +0000

    add filter by date

--- sql/select.sql
+++ sql/select.sql
@@ -1,3 +1,5 @@
+DECLARE @date AS DATETIME = '2017-08-29'
+
 SELECT u.first_name, u.last_name,
     q.id, q.question,
     a.id, a.user_login, a.answer
@@ -6,4 +8,5 @@ LEFT JOIN questions q
   ON u.username = q.user_login
 LEFT JOIN answers a
   ON q.id = a.question_id
+WHERE q.created_at >= @date
 ORDER BY u.last_name
