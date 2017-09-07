d6dc84999c48758df72dd9ac1f6acd6945014408
Jeff Puckett <jeff@jeffpuckett.com>
2017-09-07 16:29:29 +0000

    fix leaderboard user one answer per question

--- sql/leaderboard.sql
+++ sql/leaderboard.sql
@@ -1,25 +1,34 @@
 SELECT user_login, SUM(points) AS 'total'
 FROM (
-    SELECT question, a.*,
-        RANK() OVER (PARTITION BY q.id ORDER BY a.updated_at DESC) AS 'points'
-    FROM questions q
-    JOIN answers a
-      ON q.id = a.question_id
-    WHERE q.user_login = 'GACL\jpucket'
-      AND q.id > 75
-      AND a.invalid = 0
+    SELECT *, RANK() OVER (PARTITION BY qid ORDER BY updated_at DESC) AS 'points'
+    FROM (
+        SELECT q.id AS 'qid', question, a.*,
+            ROW_NUMBER() OVER (PARTITION BY q.id, a.user_login ORDER BY a.updated_at DESC) AS 'row_num'
+        FROM questions q
+        JOIN answers a
+          ON q.id = a.question_id
+        WHERE q.user_login = 'GACL\jpucket'
+          AND q.id > 75
+          AND a.invalid = 0
+    ) subt
+    WHERE row_num = 1
 ) t
 GROUP BY user_login
 ORDER BY 'total' DESC
 
 -- reverse rank highest number is best
 -- the more followers, the greater the score
-SELECT question, a.*,
-    RANK() OVER (PARTITION BY q.id ORDER BY a.updated_at DESC) AS 'points'
-FROM questions q
-JOIN answers a
-  ON q.id = a.question_id
-WHERE q.user_login = 'GACL\jpucket'
-  AND q.id > 75
-  AND a.invalid = 0
-ORDER BY q.id DESC, 'points' DESC
+SELECT qid, question, id AS 'aid', answer, user_login, updated_at,
+    RANK() OVER (PARTITION BY qid ORDER BY updated_at DESC) AS 'points'
+FROM (
+    SELECT q.id AS 'qid', question, a.*,
+        ROW_NUMBER() OVER (PARTITION BY q.id, a.user_login ORDER BY a.updated_at DESC) AS 'row_num'
+    FROM questions q
+    JOIN answers a
+      ON q.id = a.question_id
+    WHERE q.user_login = 'GACL\jpucket'
+      AND q.id > 75
+      AND a.invalid = 0
+) subt
+WHERE row_num = 1
+ORDER BY qid DESC, 'points' DESC
