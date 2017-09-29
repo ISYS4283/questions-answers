315437cc32b4cca4ff0445c9288db72590ab475d
Jeff Puckett <jeff@jeffpuckett.com>
2017-09-29 19:54:24 +0000

    test can crud answer

--- tests/QuestionTest.php
+++ tests/QuestionTest.php
@@ -34,4 +34,35 @@ class QuestionTest extends TestCase
         $delete = "DELETE FROM questions WHERE id = $id";
         $this->assertSame(1, Database::student()->exec($delete));
     }
+
+    public function test_student_crud_answer()
+    {
+        // seed question
+        $question = "What is DML?";
+        $insert = "INSERT INTO questions (question) VALUES ('$question')";
+        $this->assertSame(1, Database::admin()->exec($insert));
+        $qid = Database::admin()->lastInsertId();
+
+        // INSERT
+        $expected = $answer = "Data Manipulation Language";
+        $insert = "INSERT INTO answers (answer, question_id) VALUES ('$answer', $qid)";
+        $this->assertSame(1, Database::student()->exec($insert));
+        $aid = Database::student()->lastInsertId();
+
+        // SELECT
+        $select = "SELECT answer FROM answers WHERE id = $aid";
+        $actual = Database::student()->query($select)->fetch(PDO::FETCH_ASSOC)['answer'];
+        $this->assertSame($expected, $actual);
+
+        // UPDATE
+        $expected = $answer = "SQL to manipulate records";
+        $update = "UPDATE answers SET answer = '$answer' WHERE id = $aid";
+        $this->assertSame(1, Database::student()->exec($update));
+        $actual = Database::student()->query($select)->fetch(PDO::FETCH_ASSOC)['answer'];
+        $this->assertSame($expected, $actual);
+
+        // DELETE
+        $delete = "DELETE FROM answers WHERE id = $aid";
+        $this->assertSame(1, Database::student()->exec($delete));
+    }
 }
