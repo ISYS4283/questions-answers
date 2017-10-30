6c1ef0e42af052ccfcb01bc703378fee3aa2ee8d
Jeff Puckett <jeff@jeffpuckett.com>
2017-10-30 21:21:00 +0000

    implement post grades to blackboard

--- scripts/post-grades-to-blackboard.php
+++ scripts/post-grades-to-blackboard.php
@@ -1,9 +1,30 @@
 <?php
 
+if (!isset($argv[1], $argv[2], $argv[3])) {
+    die('question date, answer date, and chapter number are required.'.PHP_EOL);
+}
+$qdate = $argv[1];
+$adate = $argv[2];
+$chapter = $argv[3];
+
+use isys4283\qa\tests\utilities\Database;
 use razorbacks\blackboard\rest\Api;
 
 require_once __DIR__.'/../vendor/autoload.php';
 (new \Dotenv\Dotenv(dirname(__DIR__)))->load();
+
+// get scores
+$sql = "EXEC qascore @qdate='$qdate', @adate='$adate'";
+$grades = Database::student()->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+if (empty($grades)) {
+    die('qascore returned no grades.'.PHP_EOL);
+}
+
+// create bb assignment
+$courseId = getenv('BB_REST_API_COURSE_ID');
+if (empty($courseId)) {
+    die('BB_REST_API_COURSE_ID is empty.'.PHP_EOL);
+}
 
 $blackboard = new Api(
     getenv('BB_REST_API_SERVER'),
@@ -11,7 +32,21 @@ $blackboard = new Api(
     getenv('BB_REST_API_SECRET')
 );
 
-$courseId = getenv('BB_REST_API_COURSE_ID');
-$users = $blackboard->get("/courses/{$courseId}/users");
+$gradeColumn = [
+    'name' => "QA $chapter",
+    'score' => [
+        'possible' => 2,
+    ],
+    'availability' => [
+        'available' => 'Yes',
+    ],
+];
+$gradeColumn = $blackboard->post("/courses/{$courseId}/gradebook/columns", $gradeColumn);
 
-print_r($users);
+// iterate over scores and patch grades
+foreach ($grades as $grade) {
+    $endpoint = "/courses/$courseId/gradebook/columns/{$gradeColumn['id']}/users/userName:{$grade['username']}";
+    $blackboard->patch($endpoint, [
+        'score' => $grade['score'],
+    ]);
+}
